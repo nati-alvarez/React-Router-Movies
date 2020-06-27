d684d7b5059093f907202649b5db9d0bee832982
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-27 08:24:48 -0400

    Turned saved movie list into Links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, useRouteMatch} from "react-router-dom";
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <Link to="/">
-      <div className="home-button">Home</div>
-    </Link>
-  </div>
-);
+const SavedList = props => {
+  const {url} = useRouteMatch();
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <Link to={`${url}movies/${movie.id}`} className="saved-movie">{movie.title}</Link>
+      ))}
+      <Link to="/">
+        <div className="home-button">Home</div>
+      </Link>
+    </div>
+  )
+};
 
 export default SavedList;
